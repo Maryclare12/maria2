7d9a1e9e0c86f41bc5416ceb75d9ba175a5c1667
Maryclare12 <00001092916088sp@al.educacao.sp.gov.br>
2024-09-11 17:40:46 -0300

    mariaaa

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const avanva = document.querySelectorAll(".btn-proximo");
+const avanca = document.querySelectorAll(".btn-proximo");
 
-avanva.forEach(button => {
+avanca.forEach(button => {
     button. addEventListener ("click" , function(){
       const atual = document.querySelector ("ativo");
       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
       atual.classList.remove('ativo');
-      document.getElementById(proximoPasso) .classList.add('ativo');
+      document.getElementById(proximoPasso) .classList.add('ativo')
+      
+
+
     })
 })
